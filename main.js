993987ef82d851cfd66c88652ce52bc86034a4eb
LeonelHolguin <Leonelinf16@gmail.com>
2023-02-03 19:14:12 -0400

    Finish the project

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ const menu = document.querySelector(".menu");
 
 
 buttonOpen.addEventListener('click', ()=> {
-    menu.classList.remove("disabled");
-    menu.classList.add("active");
-    buttonClose.classList.remove("hidden");
-    buttonClose.classList.add("show");
+    menu.classList.remove("menu-disabled");
+    menu.classList.add("menu-active");
+    buttonClose.classList.remove("button-hidden");
+    buttonClose.classList.add("button-show");
 })
 
 buttonClose.addEventListener('click', ()=> {
-    menu.classList.remove("active");
-    menu.classList.add("disabled");
-    buttonClose.classList.remove("show");
-    buttonClose.classList.add("hidden");
+    menu.classList.remove("menu-active");
+    menu.classList.add("menu-disabled");
+    buttonClose.classList.remove("button-show");
+    buttonClose.classList.add("button-hidden");
 })
